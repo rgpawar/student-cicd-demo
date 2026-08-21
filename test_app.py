0d91ce8ccae2d199784ee83c385d80fbdc7a8ce3
Your Name <rgpawar13@gmail.com>
2026-08-21 12:22:08 +0530

    Add CI CD workflow

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 from app import predict_result
 def test_pass_student():
-    assert predict_result(75) == "PASS"
+    assert predict_result(75) == "FAIL"
 
 def test_fail_student():
     assert predict_result(30) == "FAIL"
