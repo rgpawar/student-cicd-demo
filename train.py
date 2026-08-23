3045c2803a1a0ef4ea8b7f605b99df20a1c9bf00
Your Name <rgpawar13@gmail.com>
2026-08-23 14:41:24 +0530

    Initial V1

--- train.py
+++ train.py
@@ -33,7 +33,15 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 
 # Train model
-model = DecisionTreeClassifier(
+'''model = DecisionTreeClassifier(
+    max_depth=3,
+    random_state=42
+)'''
+from sklearn.ensemble import GradientBoostingClassifier
+
+model = GradientBoostingClassifier(
+    n_estimators=100,
+    learning_rate=0.1,
     max_depth=3,
     random_state=42
 )
